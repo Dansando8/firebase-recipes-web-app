19ef3b74b8a74a9181068db40ea1af0f80e70583
Dansando8 <d.sandoval@vertciel.ca>
2022-07-19 20:04:35 -0400

    Secret

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,10 +4,7 @@ import FirebaseAuthService from '../FirebaseAuthService';
 function LoginForm({ existingUser }) {
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
-
-  useEffect(() => {
-    console.log(existingUser); 
-  }, [existingUser])
+  const [error, setError] = useState('');
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -16,13 +13,36 @@ function LoginForm({ existingUser }) {
       setUserName('');
       setPassword('');
     } catch (error) {
-      alert(error.message);
+      setError(error.message);
+      console.log(error);
     }
   }
 
   function handleLogout() {
     FirebaseAuthService.logoutUser();
   }
+
+  async function handleSendResetPasswordEmail() {
+    if (!username) {
+      setError('Missing Username!');
+      return;
+    }
+
+    try {
+      await FirebaseAuthService.sendPasswordResetEmail(username);
+      setError('The password reset email has been sent!');
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  // async function handleLoginWithGoogle() {
+  //   try {
+  //     await FirebaseAuthService.loginWithGoogle();
+  //   } catch (error) {
+  //     setError(error.message);
+  //   }
+  // }
 
   return (
     <div className="login-form-container">
@@ -40,7 +60,7 @@ function LoginForm({ existingUser }) {
       ) : (
         <form onSubmit={handleSubmit} className="login-form">
           <label className="input-label login-label">
-            Email:
+            Username (email):
             <input
               type="email"
               value={username}
@@ -60,7 +80,28 @@ function LoginForm({ existingUser }) {
             ></input>
           </label>
           <div className="button-box">
-            <button className="primary-button">Submit</button>
+            <button className="primary-button" type="submit">
+              Login
+            </button>
+            {/* <button
+              className="primary-button"
+              type="button"
+              onClick={handleLoginWithGoogle}
+            >
+              Login with Google
+            </button> */}
+            <button
+              className="primary-button"
+              type="button"
+              onClick={handleSendResetPasswordEmail}
+            >
+              Reset Password
+            </button>
+          </div>
+          <div>
+            <p style={{ fontSize: '12px', color: 'red' }}>
+              {error ? error : ''}
+            </p>
           </div>
         </form>
       )}
